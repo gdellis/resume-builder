59abcc0cdb6909fbf9a27ad1eab0dd5406d6f218
Glenn Ellis <gdellis@pm.me>
2026-03-11 19:49:21 -0600

    fix: add open-next.config.ts for Cloudflare build

---
+++ open-next.config.ts
@@ -0,0 +1,6 @@
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+
+export default defineCloudflareConfig({
+  // Optional: Configure your Cloudflare setup here
+  // See: https://opennext.js.org/cloudflare/config
+});
